e70d188c9a2e0283dee5b2fefaecacd2794e1d3b
vitalii <vitalii.vorobij@gmail.com>
2019-06-12 09:31:53 +0300

    Added UnaryExpression

---
+++ src/com/kisslang/source/parser/ast/UnaryExpression.java
@@ -0,0 +1,26 @@
+package com.kisslang.source.parser.ast;
+
+public class UnaryExpression implements Expression {
+
+    private final Expression expr1;
+
+    private final char operation;
+
+    public UnaryExpression(char operation,Expression expr1){
+        this.operation=operation;
+        this.expr1=expr1;
+    }
+
+    public double eval(){
+        if(operation=='-'){
+            return 0-expr1.eval();
+        }
+        else{
+            return expr1.eval();
+        }
+    }
+
+    public String toString(){
+        return Double.toString(expr1.eval());
+    }
+}
