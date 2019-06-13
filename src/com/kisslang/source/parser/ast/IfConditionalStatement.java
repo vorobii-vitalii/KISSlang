76a635a444682f7f722e99a753e5df45e552416b
vitalii <vitalii.vorobij@gmail.com>
2019-06-13 12:35:50 +0300

    Added support of if else statements

--- src/com/kisslang/source/parser/ast/IfConditionalStatement.java
+++ src/com/kisslang/source/parser/ast/IfConditionalStatement.java
@@ -1,7 +1,36 @@
 package com.kisslang.source.parser.ast;
 
-public abstract class IfConditionalStatement implements Statement {
+public class IfConditionalStatement implements Statement {
+
+    private final Expression expression;
+    private final Statement ifStatement;
+    private final Statement elseStatement;
+
+    public IfConditionalStatement(Expression expression,Statement ifStatement,Statement elseStatement){
+        this.expression=expression;
+        this.ifStatement=ifStatement;
+        this.elseStatement=elseStatement;
+    }
 
 
+    @Override
+    public void execute() {
 
+        final double result=expression.eval().asDouble();
+
+        if(result==1){
+            ifStatement.execute();
+        }
+        else{
+            if(elseStatement!=null) {
+                elseStatement.execute();
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "If : "+ifStatement.toString()+"\nelse :"+elseStatement.toString();
+    }
 }
